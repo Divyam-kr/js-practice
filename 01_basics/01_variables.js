3bce597c3a92ca79fe87c8497354c90d7c9175df
Divyam Kumar <122778685+Divyam-kr@users.noreply.github.com>
2025-03-19 19:10:33 +0000

    variables and datatype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,11 @@ let name = "Divyam"
 var password = "123"
 // accountId=34
 console.log(accountId);
+
+accountCity="BLR"
 name="Divyam2"
+password="21"
 console.log(name);
+console.table([name,accountId,password,accountCity])
 
 
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer versions
+
+console.log(3+3)
+
+let name = "Divyam"
+let age = 22
+let isLoggedIn = false
+
+// number, 
+//bigInt
+// string
+// boolean
+// null - standalone value
+//undefined
+// symbol -> uniqueness
+
+
+//object
+console.log(typeof age);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
