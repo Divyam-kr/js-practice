6c8e4a3f02c50ce887e3aefde37a801898d7983c
Divyam Kumar <122778685+Divyam-kr@users.noreply.github.com>
2025-03-19 19:22:56 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" ->33
+// "33abc" -> NaN
+
+let isLoggedIn = ""
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(valueInBoolean);
+
+
+//1 -> true; 0 -> false
+// "" -> false
+//"hi" -> true
